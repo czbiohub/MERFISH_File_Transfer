a69d1595fb821d9f07e623006b7c4e129ef0ebc2
Samuel Rohan D'Souza <samuelrohandsouza@gmail.com>
2022-05-31 12:53:07 -0700

    blah' git

--- transfer.sh
+++ transfer.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-export MERFISH_OUTPUT='Z:\merfish_output'
-export MERFISH_RAW_DATA='Z:\merfish_raw_data'
-export MERFISH_ANALYSIS='Z:\merfish_analysis'
+export MERFISH_OUTPUT='Z:/merfish_output'
+export MERFISH_RAW_DATA='Z:/merfish_raw_data'
+export MERFISH_ANALYSIS='Z:/merfish_analysis'
 
-ls $MERFISH_ANALYSIS
+export OUTPUT_COPY='Y:/merfish_output'
+export RAW_DATA_COPY='Y:/merfish_raw_data'
+export ANALYSIS_COPY='Y:/merfish_analysis'
+
+# Checking for files in MERFISH_OUTPUT not in OUTPUT_COPY
+diff $MERFISH_OUTPUT $OUTPUT_COPY > diff.txt
+
+
+awk "{if(/Only in $MERFISH_OUTPUT:/) print \$2}" < diff.txt > files_to_copy.txt
+
+while read file; do
+  echo "$files"
+done <files_to_copy.txt
